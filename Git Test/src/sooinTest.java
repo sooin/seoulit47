636615821116e549b319af825ec00af208368999
Sooin <Sooin@Sooin-PC>
2016-08-22 20:39:39 +0900

    200113

--- Git Test/src/sooinTest.java
+++ Git Test/src/sooinTest.java
@@ -3,7 +3,7 @@ public class sooinTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("add println");
+		System.out.println("add println¼öÀÎ");
 	}
 
 }
